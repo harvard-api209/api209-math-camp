e9f96e276c18de386d7b51982c17fe023b12bd14
Rony Rodrigo Maximiliano Rodriguez-Ramirez <maximiliano.rod.ra@gmail.com>
2024-08-10 00:18:57 -0400

    :pencil: render adjustment

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-REMOTE_HOST="ath-cloud"
-REMOTE_DIR="~/sites/classes/datavizs24.classes/public"
-REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
-
-echo "Uploading new changes to remote server..."
-echo
-rsync -crvP --delete _site/ $REMOTE_DEST
